5a61caa3a9ab6cb5dc02ce9bce9b4c899da7ab71
DAVID_RODRIGUEZ_JURADO <a.21308051280543@cbtis128.edu.mx>
2024-02-29 20:14:50 +0000

    version2

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const Miapptabbar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Miapptabbar extends StatelessWidget {
+  const Miapptabbar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo tabbar",
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blueAccent,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Mipaginainicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Mipaginainicial extends StatefulWidget {
+  const Mipaginainicial({Key? key}) : super(key: key);
 
   @override
+  State<Mipaginainicial> createState() => _MipaginainicialState();
+}
+
+class _MipaginainicialState extends State<Mipaginainicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+            title: Text("Ford Proyecto b David rodriguez "),
+            bottom: const TabBar(tabs: [
+              Tab(
+                icon: Icon(Icons.car_repair_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.car_crash),
+              ),
+              Tab(
+                icon: Icon(Icons.car_repair_rounded),
+              ),
+              Tab(
+                icon: Icon(Icons.car_rental_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.car_repair_rounded),
+              ),
+            ] //Texto icono,
+                )),
+        body: TabBarView(children: const <Widget>[
+          Center(
+              child: Icon(
+            Icons.car_repair_outlined,
+            size: 350,
+            color: Color(0xff4de91e),
+            semanticLabel: 'Text to announce in accessibility modes',
+          )),
+          Center(
+              child: Icon(
+            Icons.car_crash,
+            size: 350,
+            color: Color(0xff205e0d),
+            semanticLabel: 'Text to announce in accessibility modes',
+          )),
+          Center(
+              child: Icon(
+            Icons.car_repair_rounded,
+            size: 350,
+            color: Color(0xff1ea9e9),
+            semanticLabel: 'Text to announce in accessibility modes',
+          )),
+          Center(
+              child: Icon(
+            Icons.car_rental_outlined,
+            size: 350,
+            color: Color(0xffd04708),
+            semanticLabel: 'Text to announce in accessibility modes',
+          )),
+          Center(
+              child: Icon(
+            Icons.car_repair_rounded,
+            size: 350,
+            color: Color(0xff0040b7),
+            semanticLabel: 'Text to announce in accessibility modes',
+          )),
+        ]),
       ),
     );
   }
